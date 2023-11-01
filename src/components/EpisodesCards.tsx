3921a00ff67a3d2048e37c1dadb61adf926dfe4b
lucadumitru <luca.dumitru25@gmail.com>
2023-11-01 19:24:11 +0100

    update CharactersCards component

--- src/components/EpisodesCards.tsx
+++ src/components/EpisodesCards.tsx
@@ -1,20 +1,16 @@
 import type { IEpisode } from "@/utils/types/types";
 
 import { EpisodeCard } from "./EpisodeCard";
-import { Grid, SkeletonCards } from "./ui";
+import { Grid } from "./ui";
 
 interface EpisodeCardsProps {
   allEpisodes: IEpisode[];
-  isLoading: boolean;
 }
 
-export const EpisodesCards: React.FC<EpisodeCardsProps> = ({ allEpisodes, isLoading }) =>
-  isLoading ? (
-    <SkeletonCards variants="episodes" />
-  ) : (
-    <Grid>
-      {allEpisodes.map((episode) => (
-        <EpisodeCard key={episode.id} episode={episode} />
-      ))}
-    </Grid>
-  );
+export const EpisodesCards: React.FC<EpisodeCardsProps> = ({ allEpisodes }) => (
+  <Grid>
+    {allEpisodes.map((episode) => (
+      <EpisodeCard key={episode.id} episode={episode} />
+    ))}
+  </Grid>
+);
